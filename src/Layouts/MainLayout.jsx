0624cc7c68ecc992b72ef5d23bc1f431fb467621
emran40989 <emran40989@gmail.com>
2025-10-12 11:17:14 +0600

    structure created

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+};
+
+export default MainLayout;
