3cd06955ac33613fe9ea84f12e4e6e44eba47a47
emran40989 <emran40989@gmail.com>
2025-10-12 11:39:13 +0600

    skeleton added with error page

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Navbar from '../Components/Navbar';
-import Footer from '../Components/Footer';
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,25 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home";
+import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: "/",
-        element: <App />,
-    }
-])
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 export default router;
