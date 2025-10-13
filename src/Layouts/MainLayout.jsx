0f703fedf1db5285945e223d8a816bb85d27c363
emran40989 <emran40989@gmail.com>
2025-10-13 21:53:43 +0600

    navbar added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="main-layout">
       <Navbar />
       <Outlet />
       <Footer />
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useRouteError } from 'react-router';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>Error Page</h1>
-        </div>
-    );
+    const error = useRouteError();
+    return <>
+    <Navbar />
+    <h1>404, Page Not Found</h1>
+    <div>{error.message}</div>
+    <Footer />
+    </>
 };
 
 export default ErrorPage;
